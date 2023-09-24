654cd57613ffd3ebdf28d6b414f2f657f13dfd84
Victor Giovannoni <vernalhav@gmail.com>
2023-09-24 12:13:32 -0300

    update dashboard to include hotfix graph

--- repostats/dashboard/dashboard.py
+++ repostats/dashboard/dashboard.py
@@ -14,27 +14,98 @@ class Dashboard:
 
         repos = list(set(metric.repo for metric in self.provider.metrics))
         self.app.layout = html.Div(
-            [
+            className="container",
+            children=[
                 html.Div(children="Github Repository Metrics"),
                 html.Hr(),
-                dcc.Checklist(options=repos, value=repos, id="dropdown"),
-                dcc.Graph(figure={}, id="graph"),
-            ]
+                dcc.Checklist(options=repos, value=repos, id="files-dropdown"),
+                dcc.Graph(
+                    id="files-graph",
+                    config={
+                        "displayModeBar": False,
+                        "scrollZoom": False,
+                    },
+                ),
+                html.Hr(),
+                dcc.Dropdown(
+                    options=repos,
+                    value=repos[0],
+                    id="hotfix-dropdown",
+                    clearable=False,
+                ),
+                html.Div(
+                    className="columns centered",
+                    children=[
+                        html.Div(
+                            className="two columns centered",
+                            children=html.Div(
+                                children=[
+                                    html.H1(
+                                        0,
+                                        id="avg-hotfixes",
+                                        style={"margin": 0, "text-align": "center"},
+                                    ),
+                                    html.H6(
+                                        "hotfixes",
+                                        style={"margin": 0, "text-align": "center"},
+                                    ),
+                                    html.P(
+                                        "per release",
+                                        style={"margin": -10, "text-align": "center"},
+                                    ),
+                                ]
+                            ),
+                        ),
+                        dcc.Graph(
+                            className="ten columns",
+                            id="hotfix-graph",
+                            config={
+                                "displayModeBar": False,
+                                "scrollZoom": False,
+                            },
+                        ),
+                    ],
+                ),
+            ],
         )
 
-    @callback(Output("graph", "figure"), Input("dropdown", "value"))
-    def update_graph(self, value):
-        value = value or []
-        value = value if not isinstance(value, str) else [value]
-        data = self.provider.get_files_changed(value)
+    @callback(Output("files-graph", "figure"), Input("files-dropdown", "value"))
+    def update_files_graph(self, repos):
+        repos = repos or []
+        repos = repos if not isinstance(repos, str) else [repos]
+        data = self.provider.get_files_changed(repos)
         fig = px.box(
             data,
             orientation="h",
             x="changed_files",
             y="repo",
         )
+        fig.update_xaxes(fixedrange=True)
+        fig.update_yaxes(fixedrange=True)
         fig.update_layout(hovermode=False)
         return fig
+
+    @callback(
+        [Output("hotfix-graph", "figure"), Output("avg-hotfixes", "children")],
+        Input("hotfix-dropdown", "value"),
+    )
+    def update_hotfix_graph(self, repo):
+        data = self.provider.get_hotfixes_per_release(repo)
+        fig = px.bar(
+            data,
+            x="release",
+            y="hotfixes",
+        )
+        yticks = (
+            list(range(0, int(data.hotfixes.max() + 1))) if len(data.hotfixes) else [0]
+        )
+        fig.update_xaxes(fixedrange=True, type="category", showgrid=False)
+        fig.update_yaxes(
+            fixedrange=True,
+            tickvals=yticks,
+        )
+        avg = data.hotfixes.mean() if len(data.hotfixes) else 0
+        return fig, round(avg, 1)
 
 
 if __name__ == "__main__":
