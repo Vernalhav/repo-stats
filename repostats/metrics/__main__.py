2f93b6c93ff1c235196ee4c81862fd08e3400130
Victor Giovannoni <vernalhav@gmail.com>
2023-09-26 23:19:33 -0300

    add separate metrics CLI

--- repostats/metrics/__main__.py
+++ repostats/metrics/__main__.py
@@ -1,7 +1,41 @@
+import argparse
 import asyncio
 import os
+from typing import Sequence
 
 from repostats import metrics
+
+
+class Args:
+    repos: Sequence[str]
+    max_prs: int | None
+    outfile: str
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "repos",
+        metavar="repo",
+        action="extend",
+        nargs="+",
+        help="owner and name of repo to add to dashboard, e.g. vernalhav/repo-stats",
+    )
+    parser.add_argument(
+        "-n",
+        "--max-prs",
+        type=int,
+        help="max PRs to fetch per repo. If unspecified, fetches all PRs",
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        default="data/metrics.json",
+        help="json file to dump repo metrics",
+    )
+
+    return parser
 
 
 async def main() -> None:
@@ -9,10 +43,14 @@ async def main() -> None:
 
     dotenv.load_dotenv()
 
+    parser = get_parser()
+    args: Args = parser.parse_args()
+
     token = os.getenv("GITHUB_TOKEN", default="")
-    await metrics.to_json(
-        ["thebricks/MirroredSouls", "vernalhav/levada"], token, "data/metrics.json"
-    )
+
+    print(f"Downloading metrics for repos {', '.join(args.repos)}...")
+    await metrics.to_json(args.repos, token, args.outfile, args.max_prs)
+    print("Finished downloading metrics!")
 
 
 if __name__ == "__main__":
